db21679966170e8117079e2dc4608bc6ecb9cd14
Gurnoor Singh <gurnoor2203@gmail.com>
2025-11-01 23:57:01 +0530

    data ingestion added

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 from KidneyClassification import logger
-logger.info("wlcm to our log page")
+from KidneyClassification.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
+
+
+
+STAGE_NAME="Data Ingestion Stage"
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
